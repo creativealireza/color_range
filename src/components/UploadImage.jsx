a7535d1b5a19901d5dd7e2122dfbbef6283b8cfa
Alireza Rahimi <ar.dvlpr@outlook.com>
2023-06-26 17:12:16 +0330

    update UploadImage

--- src/components/UploadImage.jsx
+++ src/components/UploadImage.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import axios from 'axios';
 import PhotoCamera from '@mui/icons-material/PhotoCamera';
 import {
@@ -8,18 +9,19 @@ import {
     FormHelperText,
 } from '@mui/material';
 import LoadingButton from '@mui/lab/LoadingButton';
-import SaveIcon from '@mui/icons-material/Save';
+import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
+import { ImagePreview } from './ImagePreview';
 
-export const UploadImage = ({ image, imagePreview }) => {
+export const UploadImage = ({ image, imagePreview, loading }) => {
+    const [finalImage, setFinalImage] = useState();
+
     const convertBase64 = async (file) => {
         return new Promise((resolve, reject) => {
             const fileReader = new FileReader();
             fileReader.readAsDataURL(file);
-
             fileReader.onload = () => {
                 resolve(fileReader.result);
             };
-
             fileReader.onerror = (error) => {
                 reject(error);
             };
@@ -27,15 +29,15 @@ export const UploadImage = ({ image, imagePreview }) => {
     };
 
     function uploadImageToServer() {
-        axios.post('http://erfan.bestestofthebest.site/color_range', {
+        axios.post('http://127.0.0.1:5000/color_range', {
             image,
-          })
-          .then(function (response) {
-            console.log(response);
-          })
-          .catch(function (error) {
-            console.log(error);
-          });
+        })
+            .then(function (response) {
+                response?.data && loading(false)
+                setFinalImage(response?.data)
+            })
+            .catch(function (error) {
+            });
     }
 
     const handleProfileImage = async (event) => {
@@ -51,51 +53,75 @@ export const UploadImage = ({ image, imagePreview }) => {
                 display: "flex",
                 alignItems: "center",
             }}>
-            <Button
-                variant="outlined"
-                component="label"
-                sx={{
-                    color: "black",
-                    margin: "0 auto",
-                    marginTop: "3rem",
-                    border: "1px solid black",
 
-                    '&:hover': {
-                        borderColor: 'black',
-                        background: "rgb(0 0 0 / 10%)"
-                    }
-                }}
-                aria-describedby="profilePictureInfo"
-            >
-                <Stack direction="row" alignItems="center" spacing={2}>
-                    <PhotoCamera sx={{ marginRight: "0.4rem", color: "gray" }} />
-                    {imagePreview?.value ? "Change Photo" : "Upload Photo"}
-                    {imagePreview?.value && <Avatar alt="image" src={imagePreview?.value} sx={{border: "1px solid grey"}}/>}
+            <Stack display={"flex"} >
+                <Button
+                    variant="outlined"
+                    component="label"
+                    sx={{
+                        color: "black",
+                        margin: "0 auto",
+                        marginTop: "3rem",
+                        border: "1px solid black",
+
+                        '&:hover': {
+                            borderColor: 'black',
+                            background: "rgb(0 0 0 / 10%)"
+                        }
+                    }}
+                    aria-describedby="profilePictureInfo"
+                >
+                    <Stack direction="row" alignItems="center" spacing={2}>
+                        <PhotoCamera sx={{ marginRight: "0.4rem", color: "gray" }} />
+                        {imagePreview?.value ? "Change Photo" : "Upload Photo"}
+                        {imagePreview?.value && <Avatar alt="image" src={imagePreview?.value} sx={{ border: "1px solid grey" }} />}
+                    </Stack>
+                    <input
+                        hidden
+                        accept="image/*"
+                        type="file"
+                        onChange={handleProfileImage}
+                    />
+                </Button>
+                {!imagePreview?.value &&
+                    <FormHelperText id="profilePictureInfo" sx={{ textAlign: "center" }}>
+                        Upload your image
+                    </FormHelperText>}
+
+                <LoadingButton
+                    loading={false}
+                    loadingPosition="start"
+                    startIcon={<ArrowUpwardIcon />}
+                    variant="outlined"
+                    sx={{
+                        marginTop: "1rem",
+                        minWidth: "11rem"
+                    }}
+                    onClick={() => {
+                        uploadImageToServer()
+                        loading(true)
+                    }}
+                >
+                    Upload Image
+                </LoadingButton>
+            </Stack>
+
+            {finalImage &&
+                <Stack
+                    display={"flex"}
+                    justifyContent={'center'}
+                    alignItems={'center'}
+                    sx={{
+                        marginTop: "5rem",
+                        border: "1px solid black",
+                        width: "90%",
+                        padding: "0.2rem",
+                        borderRadius: "3px",
+                        boxShadow: "5px 5px 15px 0px rgba(0,0,0,0.5)"
+                    }}>
+                    <ImagePreview imageUrl={finalImage} />
                 </Stack>
-                <input
-                    hidden
-                    accept="image/*"
-                    type="file"
-                    onChange={handleProfileImage}
-                />
-            </Button>
-            {!imagePreview?.value && <FormHelperText id="profilePictureInfo">
-                Upload your image
-            </FormHelperText>}
-
-            <LoadingButton
-                loading={false}
-                loadingPosition="start"
-                startIcon={<SaveIcon />}
-                variant="outlined"
-                sx={{
-                    marginTop: "1rem",
-                    minWidth: "11rem"
-                }}
-                onClick={() => uploadImageToServer()}
-            >
-                Save
-            </LoadingButton>
+            }
         </FormControl>
     );
 }
