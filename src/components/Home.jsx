f3bd49c79cceba266146b21154d379d9c0a4471b
Alireza Rahimi <ar.dvlpr@outlook.com>
2023-06-26 17:11:15 +0330

    add loader and footer to homepage

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,21 +1,29 @@
 import { useState } from "react";
 import { UploadImage } from "./UploadImage";
-// import { Link } from 'react-router-dom';
 import {
-    Stack
+    Stack,
+    Box,
+    LinearProgress
 } from '@mui/material';
-
+import { Footer } from "./Footer/Footer";
 
 export const Home = () => {
-
     const [image, setImage] = useState(null);
     const [imagePreview, setImagePreview] = useState(null);
+    const [loading, setLoading] = useState(false);
 
     return (
         <Stack spacing={2}>
+            {loading &&
+                <Box sx={{ width: '100%' }}>
+                    <LinearProgress />
+                </Box>
+            }
             <UploadImage
                 image={{ value: image, setValue: setImage }}
-                imagePreview={{ value: imagePreview, setValue: setImagePreview }} />
+                imagePreview={{ value: imagePreview, setValue: setImagePreview }}
+                loading={setLoading} />
+            <Footer />
         </Stack >
     );
 }
